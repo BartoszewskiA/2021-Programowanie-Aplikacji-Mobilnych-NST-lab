10946c717bfb8e56ee60d8142550c71487da8d64
BartoszewskiA <bartoszewski.a@outlook.com>
2021-12-21 21:59:27 +0100

    21.12.2021

---
+++ lab06p01-pin/app/src/main/java/com/example/lab06p01_pin/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.lab06p01_pin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
